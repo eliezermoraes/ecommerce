e7a403038574b460c4f50e258b43efd4c9e663f6
eliezermoraes <eliezermoraes7@gmail.com>
2019-01-18 19:05:49 -0300

    criamos pageadmin

--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -12,12 +12,11 @@ class Page {
 		"data"=>[]
 	];
 
-	public function __construct($opts = array()){
+	public function __construct($opts = array(), $tpl_dir = "/views/"){
 
 		$this->options = array_merge($this->defaults, $opts);
-
 		$config = array(
-			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/",
+			"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir,
 			"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 			"debug"         => false
 		);
@@ -26,10 +25,9 @@ class Page {
 
 		$this->tpl = new Tpl();
 
-		$this->setData();
+		$this->setData($this->options["data"]);
 
 		$this->tpl->draw("header");
-
 	}
 
 	private function setData($data = array()){
